086ccbe5a3e310f8e5d3d9fc590d4e3df8411a8e
Luissmont <luispm2424@gmail.com>
2025-10-26 16:56:45 -0600

    infsong2

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,39 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { SpotifyLoginService } from './services/spotify-api/spotify-login-service';
+import { CookiesStorageService } from './services/general/cookies-storage-service';
+import { Router, NavigationEnd } from '@angular/router'; 
+import { Observable, filter, map } from 'rxjs'; 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.html',
+  standalone: false,
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('music-app');
+export class App implements OnInit{
+
+  showAudioController$: Observable<boolean>; 
+
+  constructor(
+    private _spotifyLogin: SpotifyLoginService,
+    private _cookieStorage: CookiesStorageService,
+    private _router: Router 
+  ){
+    this.showAudioController$ = this._router.events.pipe(
+      filter((event): event is NavigationEnd => event instanceof NavigationEnd),
+      map((event: NavigationEnd) => !event.urlAfterRedirects.includes('/search/'))
+    );
+  }
+
+  ngOnInit(): void {
+    if(!this._cookieStorage.exists('access_token') || !this._cookieStorage.isCookieValid('access_token')){
+        this._spotifyLogin.getAccessToken().subscribe({
+          next: () => console.log('Token de acceso obtenido y guardado.'),
+          error: (err) => console.error('Error al obtener el token:', err)
+        });
+    } else {
+      console.log('Token de acceso válido existente.');
+    }
+  }
+
 }
---
+++ src/app/song-info/song-info.spec.ts
@@ -0,0 +1,15 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { SongInfo } from './song-info';
+
+describe('SongInfo', () => {
+  let component: SongInfo;
+  let fixture: ComponentFixture<SongInfo>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [SongInfo]
+    })
+
+    
+  });
+});
